0ba1595a2456e714aceaa1e3f2fa07cb2134b161
cosmonaut <evan@moonside.games>
2020-12-11 13:58:34 -0800

    unfuck hash reader

--- crunch/hash.cpp
+++ crunch/hash.cpp
@@ -1,19 +1,19 @@
 /*
- 
+
  MIT License
- 
+
  Copyright (c) 2017 Chevy Ray Johnston
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +21,7 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
- 
+
  */
 
 #include "hash.hpp"
@@ -29,6 +29,7 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <limits>
 #include "tinydir.h"
 #include "str.hpp"
 
@@ -50,16 +51,19 @@ void HashString(size_t& hash, const string& str)
 
 void HashFile(size_t& hash, const string& file)
 {
-    ifstream stream(file, ios::binary | ios::ate);
-    streamsize size = stream.tellg();
-    stream.seekg(0, ios::beg);
-    vector<char> buffer(size + 1);
-    if (!stream.read(buffer.data(), size))
+    ifstream filestream;
+    filestream.open(file, ios::binary | ios::ate);
+    filestream.ignore(std::numeric_limits<std::streamsize>::max());
+    std::streamsize length = filestream.gcount();
+    filestream.clear();
+    filestream.seekg(0, ios::beg);
+    vector<char> buffer(length + 1);
+    if (!filestream.read(buffer.data(), length))
     {
         cerr << "failed to read file: " << file << endl;
         exit(EXIT_FAILURE);
     }
-    buffer[size] = '\0';
+    buffer[length] = '\0';
     string text(buffer.begin(), buffer.end());
     HashCombine(hash, text);
 }
@@ -68,15 +72,15 @@ void HashFiles(size_t& hash, const string& root)
 {
     static string dot1 = ".";
     static string dot2 = "..";
-    
+
     tinydir_dir dir;
     tinydir_open(&dir, StrToPath(root).data());
-    
+
     while (dir.has_next)
     {
         tinydir_file file;
         tinydir_readfile(&dir, &file);
-        
+
         if (file.is_dir)
         {
             if (dot1 != PathToStr(file.name) && dot2 != PathToStr(file.name))
@@ -84,10 +88,10 @@ void HashFiles(size_t& hash, const string& root)
         }
         else if (PathToStr(file.extension) == "png")
             HashFile(hash, PathToStr(file.path));
-        
+
         tinydir_next(&dir);
     }
-    
+
     tinydir_close(&dir);
 }
 
